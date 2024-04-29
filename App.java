209f976c3486be0aae63d490f3d24d94f802857b
brenerluciano <126249284+brenerluciano@users.noreply.github.com>
2024-04-29 13:09:59 -0300

    App.java

--- App.java
+++ App.java
@@ -5,8 +5,10 @@ import Classes.Animal;
 public class App {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        //Istanciando a classe e seus valores.
         Animal animal = new Animal("Branco", "Pitangus sulphuratus", "Bem-te-vi", 0.40);
 
+        //chamando a função Nome é Cor.
         System.out.println(animal.getNome()+ " está ferido");
         System.out.println(animal.getNome() + " está " + animal.getCor());
         scanner.close();
